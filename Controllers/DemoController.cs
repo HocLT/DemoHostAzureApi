7ccee370b459065636c4408aac39409866420606
HocLT <hdquang6@gmail.com>
2023-10-19 08:37:22 +0700

    Initial commit

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoHostAzureApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello Guy";
+        }
+
+        [HttpPost]
+        [Route("{name}")]
+        public string Welcome(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
